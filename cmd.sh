dadb91fea96f56f07d47d0ed142e607d74b013f5
olfa-dallel-enit <olfa.dallel@enit.utm.tn>
2023-11-03 16:36:06 +0000

    test

---
+++ cmd.sh
@@ -0,0 +1,24 @@
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh #error!!
+
+#rustup toolchain uninstall stable && rustup toolchain install stable
+#rustc --version
+
+#rustup target add wasm32-unknown-unknown
+
+#cargo install cosmwasm-check
+#cosmwasm-check --version
+
+#git clone https://github.com/olfa-dallel-enit/cw-plus
+#cd cw-plus
+#cw-plus $ cargo test
+
+
+##cw-plus $ 
+#cd contracts/cw1-whitelist
+##cw-plus/contracts/cw1-whitelist $ 
+#cargo wasm
+
+#cw-plus/contracts/cw1-whitelist $ 
+#cosmwasm-check ../../target/wasm32-unknown-unknown/release/cw1_whitelist.wasm
+
+
